96827ee97806e0b016ba2867bd2315ac8c231e11
AsTePukc <artmax120101@rambler.ru>
2021-11-09 00:03:56 +0300

    GenNum

--- GeneratNumber/GeneratNumber.cpp
+++ GeneratNumber/GeneratNumber.cpp
@@ -1,11 +1,96 @@
-﻿// GeneratNumber.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿// GeneratNumber.cpp :  Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы. //
 
 #include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+#define ch long long  // Директивой определил макрос ch (последвоательность символов) и его длинну
+
+using namespace std;
+
+/* Вычисление (a * b) % с учётом того, что результат может переполниться */
+
+ch multiplication(ch a, ch b, ch mod)
+{
+	ch x = 0, y = a % mod;
+	while (b > 0)
+	{
+		if (b % 2 == 1)
+		{
+			x = (x + y) % mod;
+		}
+		y = (y * 2) % mod;
+		b /= 2;
+	}
+	return x % mod;
+}
+
+/* Модульное возведение в степень */
+
+ch module(ch base, ch exponent, ch mod)
+{
+	ch x = 1;
+	ch y = base;
+	while (exponent > 0)
+	{
+		if (exponent % 2 == 1)
+			x = (x * y) % mod;
+		y = (y * y) % mod;
+		exponent = exponent / 2;
+	}
+	return x % mod;
+}
+
+/* Тест Миллера-Рабина */
+
+bool Test(ch p, int iteration)
+{
+	if (p < 2)
+	{
+		return false;
+	}
+	if (p != 2 && p % 2 == 0)
+	{
+		return false;
+	}
+	ch s = p - 1;
+	while (s % 2 == 0)
+	{
+		s /= 2;
+	}
+	for (int i = 0; i < iteration; i++)
+	{
+		ch a = rand() % (p - 1) + 1, temp = s;
+		ch mod = module(a, temp, p);
+		while (temp != p - 1 && mod != 1 && mod != p - 1)
+		{
+			mod = multiplication(mod, mod, p);
+			temp *= 2;
+		}
+		if (mod != p - 1 && temp % 2 == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "rus");
+	int iteration = 5;
+	ch num;
+	cout << "Введите число для его проверки на простоту: ";
+	cin >> num;
+	if (Test(num, iteration))
+	{
+		cout << " Число " << num << " простое " << endl;
+	}
+	else
+	{
+		cout << " Число " << num << " не простое " << endl;
+	}
+	return 0;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
